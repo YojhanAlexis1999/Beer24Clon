94f447493c24980f591fd41db98bcff3e0c8138b
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2021-06-15 17:56:59 -0500

    asasaa

--- express/src/routes/beer24.js
+++ express/src/routes/beer24.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const database = require('../database');
 
-router.get('/', (req,res) => {
+router.get('/categorias/:localizacion', async (req,res) => {
     try {
-        res.status(200).json({ msg: "aa" })
+        const { localizacion } = req.params
+        const categorias = await database.query("SELECT c.img, c.descripcion FROM categorias_local cl, categorias c WHERE c.id_categorias = cl.id_categoria AND cl.id_loca = ?", [localizacion])
+        res.status(200).json(categorias)
     } catch (error) {
         res.status(401).json({ error })
     }
